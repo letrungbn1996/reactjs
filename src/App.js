84c2c2925234aed1c275a1bccdef263ed36817a3
Trung <ldtrung@IfiSolution.local>
2019-07-15 17:42:47 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {Component} from 'react';
 import './App.css';
+import './components/TodoItem';
+import TodoItem from './components/TodoItem';
 
-function App() {
+
+class App extends Component{
+
+  constructor(){
+    super();
+    this.TodoLists = ['Test Case 1', 'Test Case 2', 'Test Case 3']; 
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h2>Test Case</h2>
+        {
+          this.TodoLists.map((item, index) => <TodoItem key = {index} tittle = {item} />)
+         }
+      
     </div>
   );
+}
 }
 
 export default App;
